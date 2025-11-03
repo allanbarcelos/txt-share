40e7ead9cd8616c7d03316370a7aca5a211c3da5
Allan Barcelos <allan@barcelos.dev>
2025-11-02 22:02:09 -0500

    socket 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HeaderComponent } from './header/header.component';
 import { InfoModalComponent } from './info-modal/info-modal.component';
 import { ShareModalComponent } from './share-modal/share-modal.component';
 
-const config: SocketIoConfig = { url: './', options: { path: '/socket.io' } }; // Substitua pela URL correta do seu servidor
+const config: SocketIoConfig = { url: '/', options: { path: './socket.io' } }; // Substitua pela URL correta do seu servidor
 
 @NgModule({
   declarations: [
